67c371dc3b22c41c738418867c1427b20b37ec05
ryanschmid <schmid.ryan.c@gmail.com>
2018-09-17 13:59:02 -0500

    syntax, variables & data types exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,45 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.print("Hello, ");
+        System.out.println("World!");
+
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Java Module";
+        System.out.println(myString);
+
+        float myNumber = 123;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+        int z = 4;
+        z += 5;
+        System.out.println(z);
+
+        int a = 3;
+        int b = 4;
+        b *= a;
+        System.out.println(b);
+
+
+        int c = 10;
+        int d =2;
+        c /= d;
+        d -= c;
+        System.out.println(c);
+        System.out.println(d);
+
+
+        byte num = 127;
+        num ++;
+        System.out.println(num);
     }
 }

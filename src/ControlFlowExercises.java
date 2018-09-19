64623bcf87ea56a212992ce7ccb4f0059da7964d
ryanschmid <schmid.ryan.c@gmail.com>
2018-09-19 11:26:43 -0500

    Exponent table complete w/ alignment

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -38,6 +38,7 @@ public class ControlFlowExercises {
 //        for (int j = 100; j >-11; j-=5) {
 //            System.out.println(j);
 //        }
+
         // FIZZBUZZ loop
         for (int y = 0; y <= 100; y++) {
             if (y % 3 == 0 & y % 5 == 0) {
@@ -53,38 +54,42 @@ public class ControlFlowExercises {
 
         // TABLE OF POWERS
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please enter an integer:");
-//        int inputNumber = scanner.nextInt();
-//        System.out.println("number | square | cubed");
-//        System.out.println("------ | ------ | ------");
-//        for (int x = 1; x <= inputNumber; x++){
-//            System.out.println(x + "     |" + x*x + "     |" + x*x*x + "     ");
+        System.out.println("What number would you like to go up to?");
+        int inputNumber = scanner.nextInt();
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("number | square | cubed");
+        System.out.println("------ | ------ | ------");
+        for (int x = 1; x <= inputNumber; x++){
+            int squared = x*x;
+            int cubed = x*x*x;
+            System.out.format("%-6s | %-6s | %-6s %n", x, squared, cubed);
+        };
+
+
+//        System.out.println("Please enter a numerical grade (0-100):");
+//        int inputGrade = scanner.nextInt();
+//        System.out.println("Do you wish to continue? [y/n].");
+//        String confirm = scanner.next();
+////          System.out.println(confirm);
+//        if (confirm.toString() == "y") {
+//            System.out.println("equal");
+//            if (inputGrade >= 88 & inputGrade <= 100) {
+//                System.out.println("Your grade is an A");
+//            } else if (inputGrade >= 80 & inputGrade <= 87) {
+//                System.out.println("Your grade is a B");
+//            } else if (inputGrade >= 67 & inputGrade <= 79) {
+//                System.out.println("Your grade is a C");
+//            } else if (inputGrade >= 60 & inputGrade <= 66) {
+//                System.out.println("Your grade is a D");
+//            } else if (inputGrade >= 0 & inputGrade <= 59) {
+//                System.out.println("Your grade is an A");
+//            } else {
+//                System.out.println("Not a valid grade");
+//            }
+//        } else {
+//            System.out.println("We respect your privacy");
 //        }
-
-
-        System.out.println("Please enter a numerical grade (0-100):");
-        int inputGrade = scanner.nextInt();
-        System.out.println("Do you wish to continue? [y/n].");
-        String confirm = scanner.next();
-//          System.out.println(confirm);
-        if (confirm.toString() == "y") {
-            System.out.println("equal");
-            if (inputGrade >= 88 & inputGrade <= 100) {
-                System.out.println("Your grade is an A");
-            } else if (inputGrade >= 80 & inputGrade <= 87) {
-                System.out.println("Your grade is a B");
-            } else if (inputGrade >= 67 & inputGrade <= 79) {
-                System.out.println("Your grade is a C");
-            } else if (inputGrade >= 60 & inputGrade <= 66) {
-                System.out.println("Your grade is a D");
-            } else if (inputGrade >= 0 & inputGrade <= 59) {
-                System.out.println("Your grade is an A");
-            } else {
-                System.out.println("Not a valid grade");
-            }
-        } else {
-            System.out.println("We respect your privacy");
-        }
 
 
     }

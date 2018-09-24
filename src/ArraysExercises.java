885e443b3b0b17cedbfe027bcd9a3a487b401da7
ryanschmid <schmid.ryan.c@gmail.com>
2018-09-24 16:03:37 -0500

    Arrays exercises 1 & 2 complete

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,47 +3,45 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-//    Person person1 = new Person("Ryan");
-//    Person person2 = new Person ("Sarah");
-//    Person person3 = new Person ("LT");
-//    Person person4 = new Person ("EJ");
-//
-//    Person[] family = [person1, person2, person3, person4];
+    public static Person[] addPerson(Person[] people, Person newPerson){
+        Person[] newPeople = Arrays.copyOf(people, people.length +1);
+        newPeople[newPeople.length-1] = newPerson;
+        return newPeople;
+    }
 
-
-
-//    family[0] = me;
-//    family[1] = wife;
-//    family[2] = sister;
-//
     public static void main(String[] args) {
-        int sizeOfFamily = 7;
-
-        Person person1 = new Person("Ryan");
-        Person person2 = new Person ("Sarah");
-        Person person3 = new Person ("LT");
-        Person person4 = new Person ("EJ");
-
-        Person[] family = new Person[sizeOfFamily];
-
-        family[0]= person1;
-        family[1]= person2;
-        family[2]= person3;
-        family[3]= person4;
-
-        for (Person familyMember: family) System.out.println(familyMember.name);
-
-
+//        int sizeOfFamily = 7;
+//
+//        Person person1 = new Person("Ryan");
+//        Person person2 = new Person ("Sarah");
+//        Person person3 = new Person ("LT");
+//        Person person4 = new Person ("EJ");
+//
+//        Person[] family = new Person[sizeOfFamily];
+//
+//        family[0]= person1;
+//        family[1]= person2;
+//        family[2]= person3;
+//        family[3]= person4;
+//
+//        for (Person familyMember: family) System.out.println(familyMember.name);
 
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers));
 
+        // FROM REVIEW //
+        Person [] people = new Person[3];
+        people[0]= new Person("Ryan");
+        people[1]= new Person("Sarah");
+        people[2]= new Person ("Pinot");
+        for (Person person : people) System.out.println(person.name);
+
+        System.out.println();
+        // added person
+        Person codeup = new Person("Codeup");
+        Person[] morePeople = addPerson(people, codeup);
+        for (Person person: morePeople) {
+            System.out.println(person.getName());
+        }
     }
-
-//    public static Person[] addPerson (String[] peopleArray, String newPerson){
-//        Person newPerson = new Person(newPerson);
-//        int arrayLength = peopleArray.length;
-//        peopleArray[arrayLength] = newPerson;
-//    }
-
 }
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,12 +1,7 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
 
-    // build the generator
-    public static void generateName(String[] array1, String[] array2) {
-        int designator = (int) Math.floor((Math.random() * array1.length) - 1);
-        int designator2 = (int) Math.floor((Math.random() * array2.length) - 1);
-        System.out.println("Here is your server name:");
-        System.out.println(array1[designator] + "-" + array2[designator2]);
-    };
 
     public static String[] adjectives = {"snowy", "rainy", "stormy", "windy", "cold", "freezing",
             "sunny", "thundering", "flooding", "blustery"};
@@ -14,10 +9,34 @@ public class ServerNameGenerator {
     public static String[] nouns = {"winter", "mountain", "lake", "river", "forest", "jungle",
             "fjord", "glacier", "summit", "valley"};
 
+    // build the generator
 
-    // cal the generator
-    public static void main(String[] args) {
-        generateName(adjectives, nouns);
+    public static String pickRandomWord(String[] words){
+        Random rand = new Random();
+        int i = rand.nextInt(words.length);
+        return words[i];
     }
 
+    public static void main(String[] args){
+    String word1 = pickRandomWord(adjectives);
+    String word2 = pickRandomWord(nouns);
+        System.out.println("Here is your server name:");
+        System.out.println(word1 + "-" + word2);
+    }
 }
+
+
+
+//    public static void generateName(String[] array1, String[] array2) {
+//        int designator = (int) Math.floor((Math.random() * array1.length) - 1);
+//        int designator2 = (int) Math.floor((Math.random() * array2.length) - 1);
+//        System.out.println("Here is your server name:");
+//        System.out.println(array1[designator] + "-" + array2[designator2]);
+//    };
+//
+//    // cal the generator
+//    public static void main(String[] args) {
+//        generateName(adjectives, nouns);
+//    }
+//
+//}

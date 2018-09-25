f55025ae5965b504aa4317566ab91e33563ed0f9
ryanschmid <schmid.ryan.c@gmail.com>
2018-09-25 08:57:45 -0500

    Input & shapes QA/ reviewed code

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -18,6 +18,14 @@ public class Circle {
         this.radius = radius;
     }
 
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
     public double getArea(){
         return (pi * radius * radius);
     }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,20 +1,19 @@
-//package shapes;
-//import util.Input;
-//
-//public class CircleApp {
-//
-//    int userRadius;
-//
-//    public static void main(String[] args) {
-//
-//    Input input = new Input();
-//    System.out.println("Please input the radius of a new circle");
-//    double radius = input.getDouble();
-//
-//    Circle circle = new Circle(radius);
-//    System.out.println("Circle area is: " + circle.getArea());
-//        System.out.print("Circle circumference is: " + circle.getCircumference());
-//    }
-//
-//
-//}
+package shapes;
+import util.Input;
+
+public class CircleApp {
+
+
+    public static void main(String[] args) {
+
+    Input input = new Input();
+    System.out.println("Please input the radius of a new circle");
+    double radius = input.getDouble();
+
+    Circle circle = new Circle(radius);
+    System.out.println("Circle area is: " + circle.getArea());
+        System.out.print("Circle circumference is: " + circle.getCircumference());
+    }
+
+
+}
